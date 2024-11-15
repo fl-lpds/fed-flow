c5258abb998eed22471691823af6ce5fe488d46e
Alireza Soleymani <alirezasoleymani1377@gmail.com>
2024-11-15 04:33:42 +0330

    test : add 1_2_4 heuristic test

--- test_app/integration/docker_compose/config.py
+++ test_app/integration/docker_compose/config.py
@@ -32,12 +32,12 @@ model_name = ''
 model_size = 1.28
 model_flops = 32.902
 total_flops = 8488192
-split_layer = [[6, 6], [6, 6]]  # Initial split layers
+split_layer = [[6, 6], [6, 6], [6, 6], [6, 6]]  # Initial split layers
 model_len = 7
 
 # FL training configration
 
-R = 100 # FL rounds
+R = 100  # FL rounds
 LR = 0.01  # Learning rate
 B = 100  # Batch size
 
@@ -64,17 +64,17 @@ SERVER_ADDR = 'server'
 SERVER_PORT = 5002
 EDGESERVER_PORT = {'edge1': 5001, 'edge2': 5005}
 
-K = 2  # Number of devices
+K = 4  # Number of devices
 G = 2  # Number of groups
 S = 1  # Number of server connecting devices
 
 # Unique clients order
 HOST2IP = {}
 EDGE_MQ_MAP = {'edge1': 'broker1', 'edge2': 'broker2'}
-CLIENTS_INDEX = {0: 'client1', 1: 'client2'}
-CLIENTS_CONFIG = {'client1': 0, 'client2': 1}
+CLIENTS_INDEX = {0: 'client1', 1: 'client2', 2: 'client3', 3: 'client4'}
+CLIENTS_CONFIG = {'client1': 0, 'client2': 1, 'client3': 2, 'client4': 3}
 EDGE_SERVER_LIST = ['edge1', 'edge2']
 EDGE_SERVER_CONFIG = {0: 'edge1', 1: 'edge2'}
-CLIENTS_LIST = ['client1', 'client2']
-EDGE_MAP = {'edge1': ['client1'], 'edge2': ['client2']}
-CLIENT_MAP = {'client1': 'edge1', 'client2': 'edge2'}
+CLIENTS_LIST = ['client1', 'client2', 'client3', 'client4']
+EDGE_MAP = {'edge1': ['client1', 'client2'], 'edge2': ['client3', 'client4']}
+CLIENT_MAP = {'client1': 'edge1', 'client2': 'edge1', 'client3': 'edge2', 'client4': 'edge2'}
