aea9c33afdf05779dc249095b39504ff1c3fe23a
sabasahban <sabaasahban@gmail.com>
2025-04-08 13:00:47 +0330

    test semi-decentral with fewer edges and clients

--- app/fl_training/flow/fed_client_flow.py
+++ app/fl_training/flow/fed_client_flow.py
@@ -61,7 +61,7 @@ def run(options_ins):
     dataset = data_utils.get_trainset()
     if True:
         part_tr = indices[int((N / K) * index): int((N / K) * (index + 1))]
-        train::_loader = data_utils.get_trainloader(dataset, part_tr, 0)
+        train_loader = data_utils.get_trainloader(dataset, part_tr, 0)
     else:
         # Non-IID (20% non-IID + 80% IID):
         total_samples = len(dataset)
