f2f6f50d7c92bd3af97aae8395e3453e3e5d6513
Alireza Soleimani <alirezasoleymani1377@gmail.com>
2025-06-08 11:43:49 +0330

    uuuh: idk

--- app/fl_training/flow/fed_server_flow.py
+++ app/fl_training/flow/fed_server_flow.py
@@ -123,9 +123,11 @@ def run_edge_based_offload(server: FedServerInterface, LR, options):
     flops_of_each_layer = {key: flops_of_each_layer[key] for key in sorted(flops_of_each_layer)}
     flops_of_each_layer = list(flops_of_each_layer.values())
 
-    test_load_on_edges_and_server = [[[config.model_len - 1, config.model_len - 1] for _ in range(config.K)]]
+    test_load_on_edges_and_server = []
     for layer in range(config.model_len - 1):
         test_load_on_edges_and_server.append([[layer, layer] for _ in range(len(config.CLIENTS_CONFIG.keys()))])
+    test_load_on_edges_and_server.append([[0, config.model_len - 1] for _ in range(len(config.CLIENTS_CONFIG.keys()))])
+    test_load_on_edges_and_server.append([[config.model_len - 1, config.model_len - 1] for _ in range(config.K)])
 
     fed_logger.info(Fore.YELLOW + f"Energy Testing: {test_load_on_edges_and_server}")
 
