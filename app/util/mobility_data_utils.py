13c4cce1cbf59ce7024a0bd8888c051b44492967
GITEX <samaa.hasani@gmail.com>
2025-07-17 18:12:22 +0330

    modified with logging added to simulate_real_time_update

--- app/util/mobility_data_utils.py
+++ app/util/mobility_data_utils.py
@@ -1,11 +1,15 @@
 import os
 import threading
 import time
+import logging
 
 import pandas as pd
 
 from app.entity.node import Node
 
+# Set up logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def load_user_data():
     csv_file = '/fed-flow/app/dataset/mobility_data/data.csv'
@@ -24,6 +28,12 @@ def simulate_real_time_update(node: Node, user_data):
                                 new_longitude=current_data['Longitude'],
                                 new_altitude=current_data['Altitude'],
                                 new_seconds_since_start=current_second)
+
+        # Add logging line here to trace mobility
+        logger.info(f"[Mobility] Time={current_second}s | Lat={current_data['Latitude']} Lon={current_data['Longitude']}")
+
+        # Optional: Use print instead if logger not visible
+        # print(f"[Mobility] Time={current_second}s | Lat={current_data['Latitude']} Lon={current_data['Longitude']}")
 
         time.sleep(1)
 
