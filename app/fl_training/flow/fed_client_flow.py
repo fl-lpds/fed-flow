f5c736cb38ac6f4dd8d2d15a8662352ae483fd50
GITEX <samaa.hasani@gmail.com>
2025-11-03 11:46:21 +0330

    edit

--- app/fl_training/flow/fed_client_flow.py
+++ app/fl_training/flow/fed_client_flow.py
@@ -62,7 +62,8 @@ def run(options_ins):
     train_loader = data_utils.get_trainloader(data_utils.get_trainset(), part_tr, 0)
 
     estimate_energy = options_ins.get("energy") == "True"
-    mobility = options_ins.get('mobility')
+    mobility = str(options_ins.get('mobility', os.getenv('MOBILITY', 'False'))).lower() == 'True'
+    fed_logger.info(f"[Mobility] flag = {mobility}")
     d2d = options_ins.get('d2d')
 
     if estimate_energy:
