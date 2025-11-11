c20ae31ea1d9f7af470fe2732f45a423760b4910
GITEX <samaa.hasani@gmail.com>
2025-11-11 14:44:21 +0330

    add log

---
+++ main_backup.py
@@ -0,0 +1,21 @@
+import argparse
+
+from app.entity.communicator import Communicator
+from app.entity.node_type import NodeType
+from app.fl_training.flow import fed_client_flow, fed_edgeserver_flow, fed_server_flow
+from app.util import input_utils
+
+parser = argparse.ArgumentParser()
+options = input_utils.parse_argument(parser)
+Communicator.purge_all_queues()
+
+if __name__ == '__main__':
+    node_type = options.get("node_type")
+    if node_type == NodeType.CLIENT:
+        fed_client_flow.run(options)
+    elif node_type == NodeType.EDGE:
+        fed_edgeserver_flow.run(options)
+    elif node_type == NodeType.SERVER:
+        fed_server_flow.run(options)
+    else:
+        raise ValueError("Node type not supported")
