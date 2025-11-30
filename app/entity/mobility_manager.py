2d82525e17d82f97372926866223c40ae6672237
GITEX <samaa.hasani@gmail.com>
2025-11-30 12:10:11 +0330

    edit func check_and_migrate_per_round

--- app/entity/mobility_manager.py
+++ app/entity/mobility_manager.py
@@ -159,8 +159,10 @@ class MobilityManager:
 
         if len(edge_clients) <= 1:
             fed_logger.info(
-                "[Mobility] Preventing migration: client %s is the last client on edge %s",
-                self.client.node_identifier, current_edge
+                "[Mobility] Preventing migration: client %s:%s is the last client on edge %s",
+                self.client.ip,
+                self.client.port,
+                current_edge,
             )
             return
 
